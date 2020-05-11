af278565b45e38745500fc8c6d36c2f92509ffc3
Prnyself <281239768@qq.com>
2020-05-11 18:39:33 +0800

    Reverse fix of empty map in template, should not compact empty hash when sign.

--- lib/qingstor/sdk/service/object.rb
+++ lib/qingstor/sdk/service/object.rb
@@ -482,7 +482,7 @@ module QingStor
                                     x_qs_encryption_customer_algorithm: '',
                                     x_qs_encryption_customer_key: '',
                                     x_qs_encryption_customer_key_md5: '',
-                                    x_qs_meta_data: {},
+                                    x_qs_meta_data: [],
                                     x_qs_storage_class: '')
         request = initiate_multipart_upload_request object_key, content_type:                       content_type,
                                                                 x_qs_encryption_customer_algorithm: x_qs_encryption_customer_algorithm,
@@ -497,7 +497,7 @@ module QingStor
                                             x_qs_encryption_customer_algorithm: '',
                                             x_qs_encryption_customer_key: '',
                                             x_qs_encryption_customer_key_md5: '',
-                                            x_qs_meta_data: {},
+                                            x_qs_meta_data: [],
                                             x_qs_storage_class: '')
         properties[:'object-key'] = object_key
         input = {
@@ -681,7 +681,7 @@ module QingStor
                      x_qs_encryption_customer_key_md5: '',
                      x_qs_fetch_if_unmodified_since: '',
                      x_qs_fetch_source: '',
-                     x_qs_meta_data: {},
+                     x_qs_meta_data: [],
                      x_qs_move_source: '',
                      x_qs_storage_class: '',
                      body: nil)
@@ -730,7 +730,7 @@ module QingStor
                              x_qs_encryption_customer_key_md5: '',
                              x_qs_fetch_if_unmodified_since: '',
                              x_qs_fetch_source: '',
-                             x_qs_meta_data: {},
+                             x_qs_meta_data: [],
                              x_qs_move_source: '',
                              x_qs_storage_class: '',
                              body: nil)

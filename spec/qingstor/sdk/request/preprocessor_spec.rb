81d48504d385b611447135f97054e37f548d5a6c
Prnyself <281239768@qq.com>
2021-02-07 11:10:35 +0800

    config: Add support for endpoint

--- spec/qingstor/sdk/request/preprocessor_spec.rb
+++ spec/qingstor/sdk/request/preprocessor_spec.rb
@@ -53,6 +53,41 @@ module QingStor
         expect(result[:request_headers][:Range]).to eq '100-'
         expect(result[:request_uri]).to eq '/test/path/to/key.txt'
       end
+
+      it 'processes qingstor request with endpoint' do
+        config = Config.new.load_default_config.update(endpoint: 'ep.qingstor.dev')
+        # Logger.set_level 'debug'
+        input = {
+          config:           config,
+          properties:       {
+            'bucket-name' => 'test',
+            'object-key'  => 'path/to/key.txt',
+            'zone'        => 'beta'
+          },
+          api_name:         'GET Object',
+          service_name:     'QingStor',
+          request_method:   'POST',
+          request_uri:      '/<bucket-name>/<object-key>',
+          request_headers:  {
+            'If-Match'          => '',
+            'If-Modified-Since' => 'Thu, 01 Sep 2016 07:30:00 GMT',
+            'Range'             => '100-'
+          },
+          request_elements: {},
+          status_code:      [
+            201
+          ],
+          request_body:     'This is body.'
+        }
+
+        result = Preprocessor.preprocess input
+
+        time_check = 'Thu, 01 Sep 2016 07:30:00 GMT'
+        expect(result[:request_endpoint]).to eq 'http://beta.ep.qingstor.dev'
+        expect(result[:request_headers][:'If-Modified-Since']).to eq time_check
+        expect(result[:request_headers][:Range]).to eq '100-'
+        expect(result[:request_uri]).to eq '/test/path/to/key.txt'
+      end
     end
   end
 end
